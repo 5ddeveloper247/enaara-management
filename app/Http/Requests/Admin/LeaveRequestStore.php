0b7c3668b18b8894977898866323a58450aba611
5ddeveloper247 <127947055+5ddeveloper247@users.noreply.github.com>
2026-06-01 13:23:40 +0500

    update

--- app/Http/Requests/Admin/LeaveRequestStore.php
+++ app/Http/Requests/Admin/LeaveRequestStore.php
@@ -42,7 +42,7 @@ class LeaveRequestStore extends FormRequest
                         $qq->whereNull('department_id')->orWhere('department_id', $departmentId);
                     })),
             ],
-            'start_date' => 'required|date|after_or_equal:today',
+            'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
             'reason' => [
                 'required',
