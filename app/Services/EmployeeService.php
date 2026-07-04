79e82e11d07a0831ac25904dd48349620ae6e2a0
5ddeveloper247 <260442469+ahsun-shapelogix@users.noreply.github.com>
2026-07-04 10:29:01 +0500

    update

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -1663,6 +1663,10 @@ class EmployeeService
     public function getTableData(array $filters = []): array
     {
         $query = Employee::query()
+            ->where(function ($q) {
+                $q->whereNull('employee_status')
+                  ->orWhere('employee_status', '!=', 'Terminated');
+            })
             ->with([
                 'department:id,name',
                 'organization:id,name',
